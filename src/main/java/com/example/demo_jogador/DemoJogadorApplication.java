cb0217223ea12cdfc95669b67a1f65e60eccf42f
Lais Kaminski Casagrande <lais.330323@alunosatc.edu.br>
2025-10-03 20:35:53 -0300

    iniciando a lista de exercícios

---
+++ src/main/java/com/example/demo_jogador/DemoJogadorApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo_jogador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoJogadorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoJogadorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demo_jogador/DemoJogadorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo_jogador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoJogadorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
